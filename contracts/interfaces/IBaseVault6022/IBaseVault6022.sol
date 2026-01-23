ef4a5296e386ebc64d3c21f2d271a7e7290f2c84
Pierre Fourny <pierre.fourny76@gmail.com>
2026-01-23 13:29:56 +0100

    refactor: move actions related methods into dedicated interface for `IBaseVault6022`

--- contracts/interfaces/IBaseVault6022/IBaseVault6022.sol
+++ contracts/interfaces/IBaseVault6022/IBaseVault6022.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.28;
 import {IBaseVault6022Events} from "./IBaseVault6022Events.sol";
 import {IBaseVault6022Errors} from "./IBaseVault6022Errors.sol";
 import {IBaseVault6022States} from "./IBaseVault6022States.sol";
+import {IBaseVault6022Actions} from "./IBaseVault6022Actions.sol";
 
 interface IBaseVault6022 is
     IBaseVault6022Events,
     IBaseVault6022Errors,
-    IBaseVault6022States
+    IBaseVault6022States,
+    IBaseVault6022Actions
 {
 }
--- contracts/interfaces/IBaseVault6022/IBaseVault6022States.sol
+++ contracts/interfaces/IBaseVault6022/IBaseVault6022States.sol
@@ -6,16 +6,6 @@ import {IBaseVault6022Errors} from "./IBaseVault6022Errors.sol";
 
 interface IBaseVault6022States {
     /**
-     * @notice Deposits the collateral into the vault.
-     */
-    function deposit() external;
-
-    /**
-     * @notice Withdraw the collateral from the vault.
-     */
-    function withdraw() external;
-
-    /**
      * @notice Returns a boolean indicating if the vault is deposited.
      */
     function isDeposited() external view returns (bool);
